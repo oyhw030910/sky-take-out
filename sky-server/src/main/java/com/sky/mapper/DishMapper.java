48b387bd6c866093aedbda145679f81f4bc6721d
oyhw030910@outlook.com <oyhw030910@outlook.com>
2024-01-16 14:19:21 +0800

    删除菜品

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -19,4 +23,12 @@ public interface DishMapper {
 
     @AutoFill(OperationType.INSERT)
     void insert(Dish dish);
+
+    Page<DishVO> pageQuary(DishPageQueryDTO dishPageQueryDTO);
+
+    @Select("select * from dish where id=#{id}")
+    Dish getDishById(Long id);
+
+    @Delete("delete from dish where id=#{id}")
+    void delete(Long id);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.mapper.MealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +31,9 @@ public class DishServiceImpl implements DishService {
     @Autowired
     DishFlavorMapper dishFlavorMapper;
 
+    @Autowired
+    MealMapper mealMapper;
+
     @Override
     @Transactional
     public void saveWithFlavor(DishDTO dishDTO){
@@ -36,5 +48,29 @@ public class DishServiceImpl implements DishService {
             dishFlavorMapper.insertBatch(flavors);
         }
 
+    }
+
+    @Override
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(),dishPageQueryDTO.getPageSize());
+        Page<DishVO> dishVOPage=dishMapper.pageQuary(dishPageQueryDTO);
+        return new PageResult(dishVOPage.getTotal(),dishVOPage.getResult());
+    }
+
+    @Override
+    @Transactional
+    public void deleteDishBatch(List<Long> ids) {
+        for (Long id : ids) {
+            Dish dish=dishMapper.getDishById(id);
+            if(dish.getStatus().equals(StatusConstant.ENABLE))throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+        }
+
+        List<Long> mealIds=mealMapper.getMealIdByDishIds(ids);
+        if(mealIds!=null&&!mealIds.isEmpty())throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
+
+        for (Long id : ids) {
+            dishMapper.delete(id);
+            dishFlavorMapper.deleteByDishId(id);
+        }
     }
 }
